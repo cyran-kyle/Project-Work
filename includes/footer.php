8bbbdbfeb83ce83a2f098ec39db580dca557d864
Cyran Kyle <79578676+cyran-kyle@users.noreply.github.com>
2025-02-03 15:17:15 +0000

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+<footer class="main-footer">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b>All rights reserved</b>
+      </div>
+      <strong>Copyright &copy; 2024 <a href="https://me.cyranx.lat/">Cyran Tech</a></strong>
+    </div>
+    <!-- /.container -->
+</footer>
